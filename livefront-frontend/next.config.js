e729c8cf026f43755896c251bbab90fef150c8b6
Yatharth Garg <ygarg020@gmail.com>
2025-07-06 17:43:17 -0400

    Minor changes

--- livefront-frontend/next.config.js
+++ livefront-frontend/next.config.js
@@ -1,11 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // turn off the in‐browser dev indicator entirely:
   devIndicators: false,
 
-  // if you were using turbopack via `--turbopack`, you can also
-  // disable it here (so you don’t have to pass --no-turbo on every run)
   experimental: {
     turbo: false,
   },
